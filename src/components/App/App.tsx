7bbbdf4efd16f9b357c9c0f2343597ed756c5dfb
Babenko Liudmyla <savelieva0509@gmail.com>
2024-09-25 23:47:36 +0300

    move container

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,13 @@
 import Header from '../Header/Header';
 import PriceList from '../PriceList/PriceList';
+import s from './App.module.scss';
 
 function App() {
   return (
-    <>
+    <div className={s.container}>
       <Header />
       <PriceList />
-    </>
+    </div>
   );
 }
 export default App;
